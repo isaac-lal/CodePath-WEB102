1d71a3219aaa8d4c79c60b09a641bcea538a912c
Isaac Lal <isaaclal124@gmail.com>
2023-08-23 20:03:52 -0400

    Imported Project 3

--- Project3-FlashcardsPart2/src/App.jsx
+++ Project3-FlashcardsPart2/src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
 import Card from './components/Card.jsx';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
+    <div className='App'>
       <div className='top'>
         <h1 className='title'>Learn Italian - Flashcards!</h1>
-        <p className='description'>Test your knowledge of Italian with these interactive flashcards:</p>
+        <p className='description'>
+          Test your knowledge of Italian with these interactive flashcards:
+        </p>
         <p className='card-count'>10 Cards</p>
       </div>
       <Card />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
